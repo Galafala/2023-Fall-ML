8973509fcac872533cfc1ccdefd48eb3409a09d3
Tzu-Ching Kuo <ben100719@gmail.com>
2023-09-28 01:53:28 +0800

    Homework2_v1

---
+++ 02Homework-1/ML_hw2_1.py
@@ -0,0 +1,114 @@
+from sklearn.linear_model import LinearRegression
+from sklearn.preprocessing import PolynomialFeatures
+import matplotlib.pyplot as plt
+import numpy as np
+from pathlib import Path
+
+ROOT = Path(__file__).parent
+
+def fit_model(x, y, d, show=False):
+    poly = PolynomialFeatures(degree=d)
+    xtrain_poly = poly.fit_transform(x)
+
+    polynomial_model = LinearRegression(fit_intercept=True)
+    polynomial_model.fit(xtrain_poly, y)
+
+    quad_r2 = polynomial_model.score(xtrain_poly, y)
+
+    if show:
+        print(f'Dinmension: {d}')
+        print(f'coef: {polynomial_model.coef_}')
+        print(f'inter: {polynomial_model.intercept_}')
+        print(f'r^2: {quad_r2}')
+
+    return (polynomial_model, quad_r2)
+
+def predict(model, x, d):
+    poly = PolynomialFeatures(degree=d)
+    x_poly = poly.fit_transform(x)
+    
+    return model.predict(x_poly)
+
+
+def draw_model_line(models, x, y, d, color='darkorange', alpha=1.0, label='', row=6):
+    model = models[0]
+    q2 = models[1]
+
+    xfit = np.linspace(min(x), max(x), 20)
+    
+    yfit = predict(model=model, x=xfit, d=d)
+    
+    plt.subplot(row,3,d)
+    plt.plot(xfit, yfit, c=color, alpha=alpha, lw=2, label=f'{label}, $R^2={q2:.2f}')
+    plt.scatter(x, y, c='gray')
+
+    plt.xlim([0, 1])
+    plt.ylim([-1, 8])
+
+    y_pd = predict(model=model, x=x, d=d)
+    residuals = y-y_pd
+    
+    # Residual Plot
+    plt.subplot(row,3,d+3)
+    plt.scatter(x, residuals, c='gray')
+    plt.axhline(0, c='red', linestyle='--')
+    plt.xlim([0, 1])
+    plt.ylim([-6, 6])
+    plt.xlabel('x')
+    plt.ylabel('Residuals')
+    plt.title(f'Residual Plot (Degree {d})')
+
+    #linearity
+    plt.subplot(row,3,d+6)
+    plt.scatter(y_pd, residuals, c='gray')
+    plt.axhline(0, c='red', linestyle='--')
+    plt.ylim([-6, 6])
+    plt.xlabel('y predict')
+    plt.ylabel('Residuals')
+    plt.title(f'Linearity (Degree {d})')
+
+    # normal distribution
+    plt.subplot(row,3,d+9)
+    plt.hist(residuals, bins='auto', edgecolor='black')
+    plt.ylim([0, 10])
+    plt.xlabel('Residuals')
+    plt.ylabel('Frequency')
+    plt.title(f'Normal Distribution (Degree {d})')
+
+    # Independence
+    plt.subplot(row,3,d+12)
+    plt.acorr(residuals.ravel())
+    plt.xlim([-12, 12])
+    plt.ylim([-0.75, 1.25])
+    plt.xlabel('Lag')
+    plt.ylabel('Autocorrelation')
+    plt.title(f'Independence (Degree {d})')
+
+
+def main():   
+    x_path = f"{ROOT}/02HW1_Xtrain"
+    y_path = f"{ROOT}/02HW1_Ytrain"
+
+    Xtrain = np.loadtxt(x_path)
+    Ytrain = np.loadtxt(y_path)
+
+    Xtrain = Xtrain.reshape(-1, 1)
+    Ytrain = Ytrain.reshape(-1, 1)
+
+    n = 18
+    plt.figure(figsize=(n,n*2))
+
+    models =[]
+
+    for d in [1, 2, 3]:
+        models.append(fit_model(Xtrain, Ytrain, d))
+
+    for d, model in enumerate(models, 1):
+        draw_model_line(model, Xtrain, Ytrain, d, color='darkorange', alpha=1.0, label='line')
+
+    plt.tight_layout()
+    plt.savefig(f"{ROOT}/hw2-1.jpg")
+
+
+if __name__ == "__main__":
+    main()
---
+++ 02Homework-1/ML_hw2_3.py
@@ -0,0 +1,69 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import statsmodels.api as sm
+from pathlib import Path
+
+from ML_hw2_1 import fit_model, predict
+
+ROOT = Path(__file__).parent
+
+def scatter_plot(x, y, position, title='', xlabel='', ylabel=''):
+    plt.subplot(3,2,position)
+    plt.scatter(x, y)
+    plt.title(title)
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
+
+
+def main():
+    maturity = [1, 3, 6, 9, 12, 15, 18, 21, 24, 30, 36, 48, 60, 72, 84, 96, 108, 120]
+    yields = [7.571, 7.931, 7.977, 7.996,  
+              8.126, 8.247, 8.298, 8.304,  
+              8.311, 8.327, 8.369, 8.462, 
+              8.487, 8.492, 8.479, 8.510, 
+              8.507, 8.404]
+    
+    maturity = np.array(maturity)
+    yields = np.array(yields)
+
+    Xtrain = maturity.reshape(-1,1)
+    Ytrain = yields.reshape(-1,1)
+
+    plt.figure(figsize=(12,12*1.5))
+
+    scatter_plot(maturity, yields, 1, xlabel="Maturity", ylabel="Yields")
+
+    models = []
+    for d in [1, 2, 3, 4, 5, 6]:
+        models.append(fit_model(Xtrain, Ytrain, d))
+
+    r2s = []
+    for _, r2 in models:
+        r2s.append(r2)
+
+    scatter_plot(np.linspace(1,6,6), r2s, 2, xlabel="Dimension", ylabel="R^2")
+
+    residuals = []
+
+    for d, model in enumerate(models, 1):
+        y_pd = predict(model=model[0], x=Xtrain, d=d)
+        residual = Ytrain-y_pd
+        residuals.append(residual)
+    
+    scatter_plot(Xtrain, residuals[3], 3, xlabel="Maturity", ylabel="4D's Residuals")
+
+    plt.subplot(3, 2, 4)
+    plt.hist(np.array(residuals).ravel())
+    plt.xlabel("Residuals")
+    plt.ylabel("Frequency")
+
+    ax = plt.subplot(3, 2, 5)
+    residuals = np.array(residuals).ravel()
+    sm.qqplot(residuals, line='45', fit=True, ax=ax)
+
+    plt.tight_layout()
+    plt.savefig(f"{ROOT}/hw2-3.jpg")
+
+
+if __name__ == "__main__":
+    main()
